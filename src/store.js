21ce8d2213bc3dba2086e5454af0e2ffdf928665
Ultiimaz <sven-tjeerd-sma@hotmail.com>
2020-10-22 13:29:19 +0200

    initial files for config and redux store setup

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import RootReducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+import {applyMiddleware, createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import Config from '/config/Config';
+export const store = Config.DEV? createStore(RootReducer,composeWithDevTools(applyMiddleware(thunkMiddleware))) :  createStore(RootReducer,applyMiddleware(thunkMiddleware));
